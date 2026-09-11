89b14dc4065503edf7e459183038257f929e1802
alinhaberle-cmyk <alinhaberle@gmail.com>
2026-09-11 10:33:04 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 let numButtonClicks = 0;
 function buttonClicked() {
     numButtonClicks = numButtonClicks + 1;
-    document.getElementById("mainDiv").textContent =
-        "Button Clicked times: " + numButtonClicks;
+    document.getElementById("mainDiv").textContent = "Button Clicked times: " + numButtonClicks;
+}
+
+function myFunction(id) {
+    var x = document.getElementById(id);
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
 }
